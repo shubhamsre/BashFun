beeedec4ece8612969f9b7dc86b53615d6e1d5aa
Shubh <36189251+shubhamsre@users.noreply.github.com>
2021-07-13 12:17:31 +0530

    Update list-only-directories.sh

--- list-only-directories.sh
+++ list-only-directories.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#The following code snippet will list all the leaf directories(a directory with no sub-directory) in any input path.
+#The following code snippet will list all the leaf directories(a directory with no sub-directory) in any input path with full path to the directory.
 
 module_dir=($(find $(pwd)/deployment/ -maxdepth 10 -type d -not -path '*/exclude-some-directory/*'))
 
