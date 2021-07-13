9a30588ca11b8833b01757d6b4fb1f9c9280d10c
Shubh <36189251+shubhamsre@users.noreply.github.com>
2021-07-13 12:14:15 +0530

    Create list-only-directories.sh
    Code snippet to list only directories with no sub-directory

---
+++ list-only-directories.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#The following code snippet will list all the leaf directories(a directory with no sub-directory) in any input path.
+
+module_dir=($(find $(pwd)/deployment/ -maxdepth 10 -type d -not -path '*/exclude-some-directory/*'))
+
+for module in ${module_dir[@]}
+  do
+    ls -Rd $module/*/ > /dev/null 2>&1
+    result=$?
+    if [[ $result != 0 ]]
+    then
+      echo $module
+    fi
+done
